8f05ab36f23549f9200c5d60aa35be9b6609304d
Anderson Nunes <anderson.nunes1988@gmail.com>
2017-07-29 15:46:11 -0300

    Rendering articles from the server

--- src/js/article/article.controller.js
+++ src/js/article/article.controller.js
@@ -1,8 +1,28 @@
+import marked from 'marked';
+
 class ArticleCtrl {
-  constructor() {
+  constructor(article, User, $sce, $rootScope) {
     'ngInject';
 
+    this.article = article;
+
+    this.currentUser = User.current;
+
+    $rootScope.setPageTitle(this.article.title);
+
+    this.article.body = $sce.trustAsHtml(marked(this.article.body, { sanitize: true }));
+
+    this.resetCommentForm();
   }
+
+  resetCommentForm() {
+    this.commentForm = {
+      isSubmitting: false,
+      body: '',
+      errors: []
+    }
+  }
+
 }
 
 
